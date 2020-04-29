b6325e2df06793ae0744b5c1aeb35fcff08b7823
ezequiel estigarribia <ezequielestigarribia@hotmail.com>
2020-04-29 12:29:38 -0300

    delete cdn

---
+++ js/index.js
@@ -0,0 +1,117 @@
+var app = new Vue({
+    el: '#app',
+    data() {
+        return {
+        loading: true,
+         iconsDay: [
+                {200: 'cloudy-rain-lightning'},
+                {201: 'cloudy-rain-lightning'},
+                {202: 'cloudy-rain-lightning'},
+                {230: 'cloudy-lightning'},
+                {232: 'cloudy-lightning'},
+                {233: 'cloudy-lightning'},
+                {300: 'snow'},
+                {301: 'snow'},
+                {302: 'snow'},
+                {500: 'rainy'},
+                {501: 'rainy'},
+                {501: 'rainy'},
+                {511: 'rainy'},
+                {520: 'rainy'},
+                {521: 'rainy'},
+                {522: 'rainy'},
+                {600: 'snow'},
+                {601: 'snow'},
+                {602: 'snow'},
+                {610: 'snow'},
+                {611: 'wind'},
+                {612: 'wind'},
+                {621: 'snow'},
+                {622: 'snow'},
+                {623: 'snow'},
+                {711: 'niebla'},
+                {721: 'niebla'},
+                {731: 'niebla'},
+                {741: 'niebla'},
+                {751: 'niebla'},
+                {800: 'clean'},
+                {801: 'clouds-sun'},
+                {802: 'clouds-sun'},
+                {803: 'clouds-sun'},
+                {804: 'cloudy'},
+                {900: 'rainy'},
+             ],
+         key: 'f3acb947fe9a4601aaca23a79e0e7bb4',
+         currentWeather: null,
+         codeWeather: null,
+         currentTime: null,
+         windSpeed: null,
+         iconSelected: null,
+         isNight: false
+        }
+    },
+    methods: { 
+        isNightHour(){
+            let hr = (new Date()).getHours();
+            return (hr > 20 || hr < 5) ? true : false;
+        },
+        isCloudy() {
+            return this.iconSelected[this.codeWeather] === 'cloudy' ? true : false;        
+        },   
+        isRainy() {
+            return this.iconSelected[this.codeWeather] === 'rainy' ? true : false;        
+        },    
+        isCloudsSun() {
+            return this.iconSelected[this.codeWeather] === 'clouds-sun' ? true : false;        
+        }, 
+        isCloudLightning() {
+            return this.iconSelected[this.codeWeather] === 'cloudy-lightning' ? true : false;        
+        }, 
+        isCloudRainLightning(){
+            return this.iconSelected[this.codeWeather] === 'cloudy-rain-lightning' ? true : false;  
+        },
+        isClean() {
+            return this.iconSelected[this.codeWeather] === 'clean' ? true : false;
+        },
+        isSnowy(){
+            return this.iconSelected[this.codeWeather] === 'snow' ? true : false;  
+        },
+        updateTime: function() {
+            axios
+                .get(`https://api.weatherbit.io/v2.0/current?city=Mar%20del%20Plata&country=Argentina&state=Buenos%20Aires&key=${this.key}`)
+                .then(response => {
+                    this.currentWeather = response.data.data[0];
+                    this.codeWeather = response.data.data[0].weather.code;
+                    this.windSpeed = Math.round(this.currentWeather.wind_spd * 3.6);
+
+                    this.iconSelected = this.iconsDay.filter((icon) => {
+                        return icon[this.codeWeather]
+                    })[0];
+                    this.loading = false;
+                })
+                .catch(error => {
+                    console.log(error)
+                    this.errored = true
+                })
+                .finally(() => this.loading = false);
+                }
+    },
+    mounted () {
+        this.isNight = this.isNightHour();
+
+        setInterval(() => {
+            let currentTime = new Date();
+            this.currentTime =  moment(currentTime).format('DD/MM/YYYY HH:mm:ss')
+        }, 1000);
+
+        this.updateTime();
+                
+
+        setInterval(() => {
+            this.updateTime();
+            this.isNight = this.isNightHour();
+        }, 300000);
+    }
+
+
+})
